d4df2fa909f5c5e3a33c96348c3292a00a236a06
Raymone Miller <miller_raymone@hotmail.com>
2026-05-31 17:42:37 -0400

    Add project files.

---
+++ RPG-Rock-Paper-Gun.cpp
@@ -0,0 +1,12 @@
+﻿// RPG-Rock-Paper-Gun.cpp : Defines the entry point for the application.
+//
+
+#include "RPG-Rock-Paper-Gun.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ RPG-Rock-Paper-Gun.h
@@ -0,0 +1,8 @@
+﻿// RPG-Rock-Paper-Gun.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
