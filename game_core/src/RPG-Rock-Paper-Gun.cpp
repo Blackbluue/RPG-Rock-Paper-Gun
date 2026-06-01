b021474f277ef0eaa6355c25aa61b464346b01a7
Raymone Miller <miller_raymone@hotmail.com>
2026-05-31 21:51:30 -0400

    Add template for test code.

--- game_core/src/RPG-Rock-Paper-Gun.cpp
+++ game_core/src/RPG-Rock-Paper-Gun.cpp
@@ -1,12 +1,2 @@
-﻿// RPG-Rock-Paper-Gun.cpp : Defines the entry point for the application.
-//
+﻿#include "RPG-Rock-Paper-Gun.h"
 
-#include "RPG-Rock-Paper-Gun.h"
-
-using namespace std;
-
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
-}
